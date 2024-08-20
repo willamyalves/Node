e49884a0627c2b81c2c87fb7a4a04ec9991a660e
Willamy Alves Gomes <willamy9100@gmail.com>
2024-08-19 22:32:13 -0300

    .

---
+++ PROJETOS/ACCOUNTS/index.js
@@ -0,0 +1,27 @@
+// Módulos externos
+const inquire = require("inquire");
+const chalk = require("chalk");
+
+// Módulos internos
+const fs = require("fs");
+
+const operation = () => {
+    inquire
+        .prompt([
+            {
+                type: list,
+                name: "operation",
+                message: "O que você precisa fazer?",
+                choices: [
+                    "Criar conta",
+                    "Consultar pedido",
+                    "Depositar",
+                    "Sair",
+                ],
+            },
+        ])
+        .then((answers) => {
+            const operationSelected = answers.operation;
+        })
+        .catch((error) => console.log(error));
+};
