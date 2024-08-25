176f77289604411eb6ac20b3e7550b27aa778267
Willamy Alves Gomes <willamy9100@gmail.com>
2024-08-25 16:21:45 -0300

    .

--- 9_SEQUELIZE/1_instalacao/index.mjs
+++ 9_SEQUELIZE/1_instalacao/index.mjs
@@ -21,8 +21,54 @@ app.set("view engine", "handlebars");
 
 app.use(express.static("public"));
 
-app.get("/", (req, res) => {
-  res.status(200).render("home");
+app.get("/user/edit/:id", async (req, res) => {
+  const id = req.params.id;
+
+  const user = await User.findOne({ raw: true, where: { id: id } });
+
+  res.status(200).render("useredit", { user });
+});
+
+app.post("/user/delete/:id", async (req, res) => {
+  const id = req.params.id;
+
+  await User.destroy({ where: { id: id } });
+
+  res.redirect("/");
+});
+
+app.get("/user/:id", async (req, res) => {
+  const id = req.params.id;
+
+  const user = await User.findOne({ raw: true, where: { id: id } });
+
+  res.render("userview", { user });
+});
+
+app.get("/users/create", (req, res) => {
+  res.status(200).render("adduser");
+});
+
+app.post("/users/create", async (req, res) => {
+  const name = req.body.name;
+  const occupation = req.body.occupation;
+  let newsletter = req.body.newsletter;
+
+  if (newsletter === "on") {
+    newsletter = true;
+  } else {
+    newsletter = false;
+  }
+
+  await User.create({ name, occupation, newsletter });
+
+  res.redirect("/");
+});
+
+app.get("/", async (req, res) => {
+  const users = await User.findAll({ raw: true });
+
+  res.status(200).render("home", { users });
 });
 
 conn
