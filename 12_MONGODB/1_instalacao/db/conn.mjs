bce0bd38eedcedd3eeec2bfb382bf844b7ea6692
Willamy Alves Gomes <willamy9100@gmail.com>
2024-08-28 03:34:27 -0300

    .

---
+++ 12_MONGODB/1_instalacao/db/conn.mjs
@@ -0,0 +1,13 @@
+import { MongoClient } from "mongodb";
+
+const uri = "mongodb://localhost:27017/testemongodb";
+
+const client = new MongoClient(uri);
+
+async function run() {
+  try {
+    await client.connect();
+  } catch (error) {
+    console.log(error);
+  }
+}
--- 12_MONGODB/1_instalacao/index.mjs
+++ 12_MONGODB/1_instalacao/index.mjs
@@ -1,1 +1,14 @@
-import express from 
+import express from "express";
+import exphbs from "express-handlebars";
+
+const app = express();
+const port = 3000;
+
+app.engine("handlebars", exphbs.engine());
+app.set("view engine", "handlebars");
+
+// Read body
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.use(express.static("public"));
